c36aa13d2f6dbc86cd143d7f59985ba4fd3f3371
Tracyee <caiyeee@outlook.com>
2022-06-06 13:43:36 -0400

    feat(init): add barebone react app component

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.less';
+
+const App: React.FC<Record<string, never>> = () => (
+  <p>Hello, world! Test ~~~</p>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.less';
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
