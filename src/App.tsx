3ccad526a234663e3be7d62b916e089bd74dd600
Tracyee <caiyeee@outlook.com>
2022-06-08 15:10:08 -0400

    feat: add a navbar and website logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainNavigation from './components/navigation/mainNav';
+import { NotFound } from './pages/404';
+import { Home } from './pages/home';
+import { Education } from './pages/education';
+import { About } from './pages/about';
+import { Projects } from './pages/projects';
+import { Experience } from './pages/experience';
+import { Coursework } from './pages/coursework';
 
 const App: React.FC<Record<string, never>> = () => (
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <Router>
+    <MainNavigation />
+    <main className="main-content">
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/coursework" element={<Coursework />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </main>
+  </Router>
 );
 
 export default App;
---
+++ src/components/navigation/mainNav.tsx
@@ -0,0 +1,178 @@
+import React, { MutableRefObject } from 'react';
+import { Transition } from '@headlessui/react';
+import { NavLink } from 'react-router-dom';
+import CDark from '../../assets/icons/letter-c-dark.svg';
+import YDark from '../../assets/icons/letter-y-dark.svg';
+import CLight from '../../assets/icons/letter-c-light.svg';
+import YLight from '../../assets/icons/letter-y-light.svg';
+
+const MainNavigation = (): JSX.Element => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <div>
+      <nav className="bg-gray-700">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <CLight className="h-8 w-8" />
+              </div>
+              <div className="flex-shrink-0">
+                <YLight className="h-8 w-8" />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <NavLink
+                    to="/home"
+                    className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Home
+                  </NavLink>
+
+                  <NavLink
+                    to="/education"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Education
+                  </NavLink>
+
+                  <NavLink
+                    to="/experience"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Experience
+                  </NavLink>
+
+                  <NavLink
+                    to="/projects"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Projects
+                  </NavLink>
+
+                  <NavLink
+                    to="/coursework"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Coursework
+                  </NavLink>
+                  <NavLink
+                    to="/about"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    About
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {() => (
+            <div className="md:hidden" id="mobile-menu">
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <NavLink
+                  to="/home"
+                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Home
+                </NavLink>
+
+                <NavLink
+                  to="/education"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Education
+                </NavLink>
+
+                <NavLink
+                  to="/experience"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Experience
+                </NavLink>
+
+                <NavLink
+                  to="/projects"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Projects
+                </NavLink>
+
+                <NavLink
+                  to="/coursework"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Coursework
+                </NavLink>
+
+                <NavLink
+                  to="/about"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  About
+                </NavLink>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+    </div>
+  );
+};
+
+export default MainNavigation;
